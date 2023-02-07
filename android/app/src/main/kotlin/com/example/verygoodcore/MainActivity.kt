889d39fcac437eede1df40b7f1e4311a62b2d82c
ChiPhanTlm <chiph@tlmcorp.vn>
2023-02-07 12:30:36 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.demo_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:demo_gallery/app/app.dart';
+import 'package:demo_gallery/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
